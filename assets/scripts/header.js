7c52c94a6c7d5df87450c46f750b4c5ddf6a40f8
janniks <jannik.siebert@gmail.com>
2018-09-08 02:04:00 +0900

    Add stickiness to nav

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,14 +1,15 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+// When the user scrolls the page, execute stickyScroll
+window.onscroll = function () { stickyScroll() };
 
 // Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
+var header = document.getElementById("site-nav");
 var sticky = header.offsetTop;
 
+console.log("Testing output");
+console.log(sticky);
+
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyScroll() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
     } else {
